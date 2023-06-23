66d19910118d2c839718845ac887c0d736501e69
Anderarita <123851437+Anderarita@users.noreply.github.com>
2023-06-23 01:11:51 -0600

    Add files via upload

---
+++ TareaForo/Tarea/Cajero.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    public class Cajero
+    {
+        private string titular;
+        private decimal saldo;
+        public Cajero( string titular)
+        {
+            Console.WriteLine("Bienvenido al cajero automático");
+            this.titular = titular;
+            saldo = 0;
+
+            bool salir = false;
+
+            do
+            {
+                Console.WriteLine("\n¿Qué acción desea realizar?");
+                Console.WriteLine("1. Depósito");
+                Console.WriteLine("2. Retiro");
+                Console.WriteLine("3. Salir");
+                Console.Write("Opción: ");
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Ingrese el monto a depositar: ");
+                        decimal montoDeposito = decimal.Parse(Console.ReadLine());
+                        Depositar(montoDeposito);
+                        Console.WriteLine("Depósito realizado con éxito.");
+                        break;
+                    case 2:
+                        Console.Write("Ingrese el monto a retirar: ");
+                        decimal montoRetiro = decimal.Parse(Console.ReadLine());
+                        Retirar(montoRetiro);
+                        Console.WriteLine("Retiro realizado con éxito.");
+                        break;
+                    case 3:
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opción inválida.");
+                        break;
+                }
+            } while (!salir);
+            Console.WriteLine("Gracias por utilizar el cajero automático. ¡Hasta luego!");
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"Titular: {titular}, Saldo: {saldo}");
+        }
+        public string Titular 
+        { 
+            get {  return titular; } 
+            set { titular = value; }
+        }
+
+        public decimal Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+        public void Depositar( decimal monto ) 
+        {
+            saldo += monto;
+        }
+
+        public virtual void Retirar( decimal monto )
+        { 
+            saldo -= monto; 
+        }
+    }
+
+    public class CuentaCorriente : Cajero
+    {
+        private decimal lineaCredito;
+        public CuentaCorriente(string titular, decimal lineaCredito) : base(titular) 
+        {
+            this.lineaCredito = lineaCredito;
+        }
+
+        public decimal LineaCredito 
+        {
+            get { return lineaCredito; }
+            set { lineaCredito = value; }
+        }
+
+        public override void Retirar(decimal monto)
+        {
+            if (monto <= Saldo + lineaCredito)
+            {
+                Saldo -= monto;
+            }
+            else
+            {
+                Console.WriteLine("No se puede retirar el monto especifico");
+            }
+        }
+    }
+
+}
---
+++ TareaForo/Tarea/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Tarea;
+
+//Console.WriteLine("Hello, World!");
+
+//Cajero cuenta1 = new Cajero("Juan Pérez");
+//cuenta1.Depositar(1000);
+//cuenta1.Retirar(500);
+//Console.WriteLine($"Titular: {cuenta1.Titular}, Saldo: {cuenta1.Saldo}");
+
+//CuentaCorriente cuenta2 = new CuentaCorriente("María Gómez", 2000);
+//cuenta2.Depositar(500);
+//cuenta2.Retirar(3000);
+//Console.WriteLine($"Titular: {cuenta2.Titular}, Saldo: {cuenta2.Saldo}, Línea de crédito: {cuenta2.LineaCredito}");
+
+Console.WriteLine("Bienvenidos al Cajero");
+Console.WriteLine("Ingrese el Nombre del Titular");
+string titular = Console.ReadLine();
+
+Console.Write("Ingrese el saldo inicial: ");
+decimal saldoInicial = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Cajero cuenta = new Cajero(titular);
+
+Console.WriteLine($"Titular: {cuenta.Titular}, Saldo disponible es de: {cuenta.Saldo + saldoInicial}");
+Console.WriteLine();
+        
+
+    
+
